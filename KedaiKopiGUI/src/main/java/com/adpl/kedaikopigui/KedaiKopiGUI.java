5c84afb01aa0df172d235995301690efcfabbbae
faiz <faizhaidzar@gmail.com>
2026-09-26 01:58:56 +0700

    Add files via upload

---
+++ KedaiKopiGUI/src/main/java/com/adpl/kedaikopigui/KedaiKopiGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.adpl.kedaikopigui;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class KedaiKopiGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
